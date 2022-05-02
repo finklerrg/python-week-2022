687bc71ea5f90b9f1ac9dfaf96d81f4709c76af8
Rodrigo Finkler <finklerrg@gmail.com>
2022-05-02 13:03:24 -0300

    Added commands add and list for the CLI

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,16 @@
-from .config import settings
+import typer
 
 
-def main():
-    print("Hello from", settings.NAME)
+main = typer.Typer(help="Beer Management Application")
+
+
+@main.command("add")
+def add(name: str, style: str):
+    """Adds a new beer to the database."""
+    print(f"Adding {name} {style}")
+
+
+@main.command("list")
+def list_beers(style: str):
+    """Lists all beers of a given style."""
+    print(f"Listing {style}")
