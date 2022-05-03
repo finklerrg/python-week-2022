0d86aedf6c8e98a7eb40952ab486acd8dc394a30
Rodrigo Finkler <finklerrg@gmail.com>
2022-05-02 22:45:00 -0300

    added sesssion and removed CLI warnings

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,7 +1,17 @@
-from sqlmodel import create_engine
-from beerlog.config import settings
 from beerlog import models
+from beerlog.config import settings
+from sqlmodel import create_engine, Session
+import warnings
+from sqlalchemy import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
 
 engine = create_engine(settings.database.url)
 
 models.SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    return Session(engine)
