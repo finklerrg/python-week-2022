060d1a8768988381a5ab3b79401772c05c173c93
Rodrigo Finkler <finklerrg@gmail.com>
2022-05-03 23:03:10 -0300

    API ready

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,13 +1,25 @@
 from typing import List
 from fastapi import FastAPI  # protocol: ASGI
 from beerlog.core import get_beers_from_database
+from beerlog.database import get_session
+from beerlog.serializers import BeerOut, BeerIn
 from beerlog.models import Beer
 
 
 api = FastAPI(title="Beerlog API", version="0.1.0")
 
 
-@api.get("/beers", response_model=List[Beer])
-def list_beers():
+@api.get("/beers", response_model=List[BeerOut])
+async def list_beers():
     beers = get_beers_from_database()
     return beers
+
+
+@api.post("/beers", response_model=BeerOut)
+async def add_beer(beer_in: BeerIn):
+    beer = Beer(**beer_in.dict())
+    with get_session() as session:
+        session.add(beer)
+        session.commit()
+        session.refresh(beer)
+        return beer
