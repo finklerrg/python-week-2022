271e62374dc436dd0e684c5b9eea4c09cf9c98b7
Rodrigo Finkler <finklerrg@gmail.com>
2022-05-07 11:13:23 -0300

    :sparkles: api supporting beer style in request

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from fastapi import FastAPI, Response, status  # protocol: ASGI
 from beerlog.core import get_beers_from_database
 from beerlog.database import get_session
@@ -10,8 +10,8 @@ api = FastAPI(title="Beerlog API", version="0.1.0")
 
 
 @api.get("/beers", response_model=List[BeerOut])
-async def list_beers():
-    beers = get_beers_from_database()
+async def list_beers(style: Optional[str] = None):
+    beers = get_beers_from_database(style)
     return beers
 
 
